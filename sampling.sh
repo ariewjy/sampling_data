067270eb352ca135f0ec0c619205df07914df529
Aditya Arie Wijaya <adtarie32305@gmail.com>
2022-05-04 10:25:55 +0800

    first commit

---
+++ sampling.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+#a
+wget -P ./data https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
+
+#b-i
+in2csv ./data/weather_data.xlsx --sheet "weather_2014">./data/weather_2014.csv
+
+#b-ii
+in2csv ./data/weather_data.xlsx --sheet "weather_2015">./data/weather_2015.csv
+
+#c
+csvstack ./data/weather_2014.csv ./data/weather_2015.csv >./data/weather.csv
+
+rm ./data/weather_data.xlsx
+
+#d
+cat ./data/weather.csv | sample --rate 0.3 > ./data/sample_weather.csv
+
+
